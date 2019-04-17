08323468313da276b94187f0a88e281700ac92c5
nemoto <nemoto@enfunctor.com>
2019-04-18 05:35:31 +0900

    feat: amplify push

---
+++ graphql/subscriptions.js
@@ -0,0 +1,27 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = `subscription OnCreateTodo {
+  onCreateTodo {
+    id
+    name
+    done
+  }
+}
+`;
+export const onUpdateTodo = `subscription OnUpdateTodo {
+  onUpdateTodo {
+    id
+    name
+    done
+  }
+}
+`;
+export const onDeleteTodo = `subscription OnDeleteTodo {
+  onDeleteTodo {
+    id
+    name
+    done
+  }
+}
+`;
